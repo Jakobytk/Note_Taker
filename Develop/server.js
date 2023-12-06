312e18743ecb29e76d635c9f3920601d23b82d36
Jacob Kueck <jacob.kueck@gmail.com>
2023-12-05 18:27:05 -0600

    app.path added and working

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 // const db = require('./db/db.json');
 // const api = require('./public/assets/js/index');
 const fs = require('fs');
+const uuid = require('./public/helpers/uuid')
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use('/api', api);
 app.use(express.static('public'));
 
-app.get('*', (req, res) => 
+app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
@@ -21,55 +22,64 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) => {
-  console.info(`${req.method} request received`);
-
-  const { title, text } = req.body;
+app.post('/api/notes', (req, res) => {
+    
+    console.info(`${req.method} request received`);
   
-  if (title, text) {
-    const newReview = {
-      title,
-      text
-    };
+    const { title, text } = req.body;
 
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedReviews = JSON.parse(data);
-        parsedReviews.push(newReview);
+    if (title && text) {
+      
+      const newReview = {
+        title,
+        text,
+        id: uuid(),
+      };
+  
+     
+      fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          
+          const parsedReviews = JSON.parse(data);
+  
+          parsedReviews.push(newReview);
+  
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedReviews, null, 2),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successful!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newReview,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting review');
+    }
+  });
 
-        fs.writeFile(
-          './db/db.json',
-          JSON.stringify(parsedReviews),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successful')
-        );
-      }
-    });
-
-    const response = {
-      status: 'success',
-      body: newReview,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error');
-  }
-});
-
-
-// app.post('/notes', (req, res) => 
-//     res.sendFile(path.join(__dirname, '/public/notes.html'))
+// app.get('/api/notes', (req, res) => 
+    
 // );
 
 // app.delete('/notes', (req, res) => 
 //     res.sendFile(path.join(__dirname, '/public/notes.html'))
 // );
+
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
 app.listen(PORT, () => 
     console.log(`app listening at http://localhost:${PORT}`)
